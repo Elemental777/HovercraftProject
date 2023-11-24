8c749584523038a8887423a5cd2373c9d254df8b
Elemental777 <99276533+Elemental777@users.noreply.github.com>
2023-11-24 16:12:13 -0500

    Update testing.ino

--- final code/testing.ino
+++ final code/testing.ino
@@ -5,16 +5,20 @@
 #include <stdio.h>
 #include <math.h>
 #include <Servo.h>
-
 #include "Wire.h"
 #include <MPU6050_light.h>
 
+//US
+#define trigPin 11  //US trigger on PB3
+#define echoPin 2  //US echo on PD2
+
+//mpu
 #define YAW_THRESHOLD 90.0
 #define SERVO_MIN_ANGLE 0
 #define SERVO_MAX_ANGLE 180
 
-#define PWM_PIN 11 // Pin 11 (D3) is PD7 for PWM control
 
+//MPU global variables
 MPU6050 mpu(Wire);
 unsigned long timer = 0;
 float yaw;
@@ -22,7 +26,18 @@ Servo myservo;
 int pwmValue;
 float xAcceleration; 
 
+//US global variables
+long duration;
+double distance;
+
+
+
+
 void setup() {
+  
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  
   Serial.begin(9600);
   Wire.begin();
   myservo.attach(9);
@@ -58,8 +73,63 @@ void loop() {
 
   
 
-//myservo.write(map(yaw, -180, 180, 180, 0)); //THIS MIGHT MAKE THE SERVO TURN THE CORRECT DIRECTION
-  map(yaw, 180, -180, 180, 0); // Modify servo angle as needed
-  myservo.write(yaw + 90); // Update the servo angle
+  
+  map(yaw, 180, -180, 0, 180); // Modify servo angle as needed
+   myservo.write(90); // Update the servo angle
+   delay(3000);
+  myservo.write(sweep()); // Update the servo angle
+
+  delay(10000);
   timer = millis();
+}
+
+
+//idea
+/* int getYaw(){
+  
+} 
+*/
+
+
+int sweep(){
+int tempTheta = 0;
+  double tempDist = 0;
+  double longestDist = 0;
+  for (int i=0; i<181; i+=2){
+  myservo.write(i);
+  delay(40);  // delays and degrees to be fine tuned
+    tempDist = USdist();
+    Serial.print("Servo angle:");
+    Serial.println(i);
+    delay(10);
+    if( tempDist > longestDist){
+      longestDist = tempDist;  
+      tempTheta = i;   //check with yaw value 
+    }
+    
+  }
+  return tempTheta;
+  
+}
+
+
+double USdist(){
+
+  digitalWrite(trigPin, LOW); //clear trig
+  delayMicroseconds(2); // Wait for 2 microseconds
+  
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10); // pulse for 10 microseconds
+  
+  digitalWrite(trigPin, LOW);
+  
+  duration = pulseIn(echoPin, HIGH);	//read input echo
+  
+  distance= duration*0.034/2;	//distance calc using 0.034cm/microsec
+  
+  
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  
+  return distance;
 }
